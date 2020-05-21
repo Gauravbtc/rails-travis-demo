97c35be147606fb3de44951e80a4a50c133fb97a
gauravmakwana <gauravm@thinkbiz.co.in>
2020-05-21 21:57:56 +0900

    post spec

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :post do
-    
+    name { "Gaurav" }
+    title {"My first post"}
+    content {"this is dummy post"}
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:post) { FactoryBot.create(:post) } 
+  describe "Post Validations" do
+    it "must contain title" do
+      post.title = nil
+      expect(post).to be_invalid 
+    end
+  end
+  
 end
